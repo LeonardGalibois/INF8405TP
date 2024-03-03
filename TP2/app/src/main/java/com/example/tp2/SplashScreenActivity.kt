5e04c3da9a3d4a6df7ecebecaa5f73ff7efca69a
MikaelPotvin <mikael.potvin@polymtl.ca>
2024-03-02 20:07:13 -0500

    Ajout Splash Screen

---
+++ TP2/app/src/main/java/com/example/tp2/SplashScreenActivity.kt
@@ -0,0 +1,20 @@
+package com.example.tp2
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.ComponentActivity
+
+class SplashScreenActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this@SplashScreenActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
