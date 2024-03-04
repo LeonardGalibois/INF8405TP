07c6323ae5a9c2b328547ec60a12948eb528a64f
MikaelPotvin <mikael.potvin@polymtl.ca>
2024-03-04 16:36:20 -0500

    Début Swap Theme

--- TP2/app/src/main/java/com/example/tp2/MainActivity.kt
+++ TP2/app/src/main/java/com/example/tp2/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.example.tp2
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.widget.ToggleButton
 import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatDelegate
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val themeButton: ToggleButton = findViewById(R.id.theme_button)
+        themeButton.text = getString(R.string.swap_theme)
+        themeButton.textOff = getString(R.string.swap_theme)
+        themeButton.textOn = getString(R.string.swap_theme)
+
+        val sharedPreferences: SharedPreferences = getSharedPreferences("MODE", Context.MODE_PRIVATE)
+        val editor: SharedPreferences.Editor = sharedPreferences.edit()
+        val darkMode: Boolean = sharedPreferences.getBoolean("dark", false)
+
+        if (darkMode) {
+            themeButton.isChecked = true
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        }
+
+        themeButton.setOnCheckedChangeListener { _, isChecked ->
+            if (!isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                editor.putBoolean("dark", false)
+            }
+            else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                editor.putBoolean("dark", true)
+            }
+            editor.apply()
+        }
     }
 }
