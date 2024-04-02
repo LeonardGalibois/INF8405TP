6ed3772eac7e635f65339dec3b1cc445c054f95d
MikaelPotvin <mikael.potvin@polymtl.ca>
2024-04-02 15:25:13 -0400

    Implémentation compteur de pas

--- tracer/app/src/main/java/com/example/tracer/HomeFragment.kt
+++ tracer/app/src/main/java/com/example/tracer/HomeFragment.kt
@@ -1,12 +1,19 @@
 package com.example.tracer
 
+import android.content.Context
 import android.content.res.ColorStateList
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
+import android.widget.TextView
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 
 // TODO: Rename parameter arguments, choose names that match
@@ -19,11 +26,18 @@ private const val ARG_PARAM2 = "param2"
  * Use the [HomeFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(), SensorEventListener {
     lateinit var toggleButton: ImageButton
     var isStarted: Boolean = false
+    private var sensorManager: SensorManager? = null
+    private var walking = false
+    private var totalSteps = 0f
+    private var previousTotalSteps = 0f
+    private lateinit var stepsTextView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        sensorManager = requireContext().getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        loadData()
     }
 
     override fun onCreateView(
@@ -31,7 +45,10 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        stepsTextView = view.findViewById(R.id.steps_text_view)
+        resetSteps()
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -59,11 +76,55 @@ class HomeFragment : Fragment() {
     }
 
     private fun start() {
-
+        onResume()
     }
 
     private fun stop() {
+        resetSteps()
+    }
 
+    override fun onResume() {
+        super.onResume()
+        walking = true
+        val stepSensor = sensorManager?.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
+
+        if (stepSensor == null) {
+            Toast.makeText(requireContext(), "No sensor detected on this device", Toast.LENGTH_SHORT).show()
+        }
+        else {
+            sensorManager?.registerListener(this, stepSensor, SensorManager.SENSOR_DELAY_UI)
+        }
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (walking) {
+            totalSteps = event!!.values[0]
+            val currentSteps = totalSteps.toInt() - previousTotalSteps
+            stepsTextView.text = "$currentSteps"
+        }
+    }
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+        TODO("Not yet implemented")
+    }
+
+    private fun resetSteps() {
+        previousTotalSteps = totalSteps
+        stepsTextView.text = "0"
+        saveData()
+    }
+
+    private fun saveData() {
+        val sharedPreferences = requireContext().getSharedPreferences("preferences", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putFloat("steps", previousTotalSteps)
+        editor.apply()
+    }
+
+    private fun loadData() {
+        val sharedPreferences = requireContext().getSharedPreferences("preferences", Context.MODE_PRIVATE)
+        val savedSteps = sharedPreferences.getFloat("steps", 0f)
+        previousTotalSteps = savedSteps
     }
 
     companion object {
