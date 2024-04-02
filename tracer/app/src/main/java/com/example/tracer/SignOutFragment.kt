c1aac68b16359a3edd4ee69c0125df3dac8e8f47
matisgregoire <matis.gregoire@aero.bombardier.com>
2024-04-02 14:54:50 -0400

    MG: Added header to navigation drawer and a sign out function

---
+++ tracer/app/src/main/java/com/example/tracer/SignOutFragment.kt
@@ -0,0 +1,37 @@
+package com.example.tracer
+
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.DialogInterface
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+
+
+class SignOutFragment : DialogFragment() {
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog =
+        AlertDialog.Builder(requireContext())
+            .setMessage(getString(R.string.sign_out_message))
+            .setPositiveButton(getString(R.string.sign_out_ok), object: DialogInterface.OnClickListener {
+                override fun onClick(dialog: DialogInterface?, which: Int) { confirm() }
+            })
+            .setNegativeButton(getString(R.string.sign_out_cancel), object: DialogInterface.OnClickListener {
+                override fun onClick(dialog: DialogInterface?, which: Int) { cancel() }
+            })
+            .create()
+
+    fun confirm()
+    {
+        val intent = Intent(getActivity(), LoginActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun cancel()
+    {
+
+    }
+}
