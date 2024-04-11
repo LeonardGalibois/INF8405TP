46ccaca4126bcae76ee282a52627de1a34a60e0c
Crimo <abdelkrim.nahi@polymtl.ca>
2024-04-11 12:09:48 -0400

    added email in nav menu

--- tracer/app/src/main/java/com/example/tracer/MainActivity.kt
+++ tracer/app/src/main/java/com/example/tracer/MainActivity.kt
@@ -12,6 +12,7 @@ import android.util.Log
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.Toolbar
@@ -26,23 +27,28 @@ import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.onNavDestinationSelected
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.navigation.NavigationView
+import com.google.firebase.auth.FirebaseAuth
 
 
 class MainActivity : AppCompatActivity() {
+    lateinit var firebaseAuth: FirebaseAuth
     lateinit var navController: NavController
     lateinit var toolbar: Toolbar
     lateinit var drawerLayout: DrawerLayout
     lateinit var navigationView: NavigationView
+    lateinit var nav_drawer_username: TextView
     val historyViewModel: HistoryViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        firebaseAuth = FirebaseAuth.getInstance()
 
         if (!isLoggedIn())
         {
             login()
         }
+
 
         // Variables
         toolbar = findViewById(R.id.toolbar)
@@ -50,6 +56,15 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
         navigationView = findViewById(R.id.nav_view)
+
+        val headerView = navigationView.getHeaderView(0)
+        nav_drawer_username = headerView.findViewById(R.id.nav_drawer_username)
+
+        val currentUser = firebaseAuth.currentUser
+        currentUser?.let {
+            nav_drawer_username.text = it.email
+        }
+
 
         // Navigation
         val appBarConfig = AppBarConfiguration(navController.graph, drawerLayout)
@@ -61,6 +76,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         val inflater: MenuInflater = menuInflater
         inflater.inflate(R.menu.menu, menu)
+
         return true
     }
 
